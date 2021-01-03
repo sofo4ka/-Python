a72078e1de88927e653a2bae726847ea851a2c03
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-01-03 18:39:15 +0300

    added 5 ex

--- 2/5.py
+++ 2/5.py
@@ -6,3 +6,21 @@
 # Пользователь ввел число 8. Результат: 8, 7, 5, 3, 3, 2.
 # Пользователь ввел число 1. Результат: 7, 5, 3, 3, 2, 1.
 # Набор натуральных чисел можно задать непосредственно в коде, например, my_list = [7, 5, 3, 3, 2].
+
+set_of_integer_numbers = [7, 5, 3, 3, 2]
+try:
+    users_input_number = int(input('Введите новый элемент: '))
+except ValueError:
+    print('Некорректный ввод')
+number_of_entries = set_of_integer_numbers.count(users_input_number)
+if number_of_entries > 0:
+    index_of_entry = number_of_entries + set_of_integer_numbers.index(users_input_number)
+    set_of_integer_numbers.insert(index_of_entry, users_input_number)
+elif number_of_entries == 0 and set_of_integer_numbers[len(set_of_integer_numbers)-1] > users_input_number:
+    set_of_integer_numbers.append(users_input_number)
+else:
+    for element in set_of_integer_numbers:
+        if element < users_input_number:
+            set_of_integer_numbers.insert(set_of_integer_numbers.index(element), users_input_number)
+            break
+print(set_of_integer_numbers)
