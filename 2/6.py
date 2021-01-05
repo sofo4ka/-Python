74736a5b6fe7a064a61c7bc007bc5f5d1ed7d198
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-01-05 17:28:28 +0300

    added 6 ex

--- 2/6.py
+++ 2/6.py
@@ -17,3 +17,47 @@
 # “количество”: [5, 2, 7],
 # “ед”: [“шт.”]
 # }
+
+
+products = [
+    (1, { 'название': 'компьютер', 'цена': 20000, 'количество': 5, 'eд': 'шт.' }),
+    (2, { 'название': 'принтер', 'цена': 6000, 'количество': 2, 'eд': 'шт.' }),
+    (3, { 'название': 'сканер', 'цена': 2000, 'количество': 7, 'eд': 'шт.' })
+]
+
+next_line_number = len(products)
+q = 0
+while q == 0:
+    next_line_number = next_line_number + 1
+    input_data = input('Введите характеристики товаров: название, цену, количество, единицу измерения через запятую: ')
+    input_list = input_data.split(',')
+    new_data = {'название': input_list[0], 'цена': int(input_list[1]), 'количество': int(input_list[2]), 'eд': input_list[3]}
+    products.append((next_line_number, new_data))
+    while True:
+        try:
+            q = int(input('Введите 1 для выхода, 0 для продолжения ввода данных:'))
+        except ValueError:
+            print('Некорректный ввод')
+        except TypeError:
+            print('Некорректный ввод')
+        finally:
+            if q == 0 or q == 1:
+                break
+print(products)
+
+
+names = []
+prices = []
+quantity = []
+units = []
+
+for i in range(0, len(products)):
+    one_product_data = products[i][1]
+    print(one_product_data)
+    names.append(one_product_data.get('название'))
+    prices.append(one_product_data.get('цена'))
+    quantity.append(one_product_data.get('количество'))
+    units.append(one_product_data.get('eд'))
+
+products_info = { 'название': names, 'цена': prices, 'количество': quantity, 'eд': units }
+print(products_info)
