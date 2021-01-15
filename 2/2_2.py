14a47feb6364f91fddf21bed2af1e95ceb85fe8b
S <33701267+sofo4ka@users.noreply.github.com>
2021-01-15 11:33:49 +0300

    Update 2_2.py
    updated after checkup

--- 2/2_2.py
+++ 2/2_2.py
@@ -6,13 +6,13 @@ spisok = [2021, 'с', 'новым', 'годом', 20.21, None]
 n = 0
 try:
     n = int(input('Введите число элементов, которым вы хотите дополнить список:'))
-except ValueError:
-    print('Некорректный ввод')
+except Exception as err:
+    print("Ошибка:", err)
 for j in range(n):
     new_data = input('Введите элемент: ')
     try:
         spisok.append(float(new_data))
-    except ValueError:
+    except print("Ошибка:", err1):
         if new_data == 'True':
             spisok.append(True)
         elif new_data == 'False':
