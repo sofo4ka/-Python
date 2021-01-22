e7c3e732e9c0d0eea7374d49a2c1d7b339e0c1e4
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-01-23 02:01:11 +0300

    1 upgr

--- 7/7_1.py
+++ 7/7_1.py
@@ -8,16 +8,14 @@
 # Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем
 # с первым элементом первой строки второй матрицы и т.д.
 from random import randint
+
+
 class Matrix:
     def __init__(self, spisok_spiskov):
         self.spisok_spiskov = spisok_spiskov
 
     def __str__(self):
-        for i in self.spisok_spiskov:
-            for j in i:
-                print(j, end=' ')
-            print(' ')
-        print(' ')
+        return str('\n'.join(['\t'.join([str(i) for i in j]) for j in self.spisok_spiskov]))
 
     def __add__(self, other):
         result_matrix = []
@@ -28,15 +26,17 @@ class Matrix:
             result_matrix.append(result_line)
         return Matrix(result_matrix)
 
+
 m = [[randint(-50, 50) for i in range(3)] for j in range(3)]
 m2 = [[randint(-50, 50) for i in range(3)] for j in range(3)]
 m3 = [[randint(-50, 50) for i in range(3)] for j in range(3)]
 test_matrix = Matrix(m)
 test_matrix2 = Matrix(m2)
 test_matrix3 = Matrix(m3)
-# test_matrix.__str__()
-# test_matrix2.__str__()
-# test_matrix3.__str__()
-
+print(test_matrix.__str__())
+print(' ')
+print(test_matrix2.__str__())
+print(' ')
+print(test_matrix3.__str__())
+print(' ')
 print(test_matrix + test_matrix2 + test_matrix3)
-c = 1
