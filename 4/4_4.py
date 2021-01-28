7a25c522bbab4fda3dfa063198b4282a6f0f4e46
sofa.shvaryova <sofa.shvaryova@gmail.com>
2021-01-29 01:21:56 +0300

    added easier versions of programs by sensei luchanos

--- 4/4_4.py
+++ 4/4_4.py
@@ -18,3 +18,10 @@ for i in generator():
     print(i)
 
 
+#вариант с list comprehension by luchanos:
+from random import randint
+
+my_list = [randint(-10, 10) for i in range(20)]
+uniq_list = [el for el in my_list if my_list.count(el) == 1]
+print(f"Source list\n{my_list}\nNo repetition list\n{uniq_list}")
+
